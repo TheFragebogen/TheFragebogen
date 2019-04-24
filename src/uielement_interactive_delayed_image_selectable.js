35aa79281f476977555f29a39c59d7150eb4820e
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2019-04-24 12:13:09 +0200

    Added UIElementInteractiveDelayedImageSelectable and a corresponding demo. Original implemention by Sebastian Schuck.

---
+++ src/uielement_interactive_delayed_image_selectable.js
@@ -0,0 +1,110 @@
+/**
+Simulates the delayed loading of an image that is selectable (via checkbox).
+During the load process a load animation (another image) is shown.
+
+DEVELOPER:
+* does not support preloading the images
+
+@class UIElementInteractiveDelayedImageSelectable
+@augments UIElement
+@augments UIElementInteractive
+
+@param {string} [className] CSS class
+@param {string} loadAnimationURL URL of the load animation.
+@param {string} imageURL URL of the image.
+@param {string} imageCaption The caption of the image.
+@param {float} loadDelay The delay in ms.
+@param {int} [readyMode=0] 0: immediately, 1: selected, 2: not selected, 3: ready on delayed load, 4: case 1 & 3; 5: case 2 & 3
+*/
+function UIElementInteractiveDelayedImageSelectable(className, loadAnimationURL, imageURL, imageCaption, imageDelay, readyMode) {
+    UIElementInteractive.call(this);
+
+    this.className = className;
+
+    this.loadAnimationURL = loadAnimationURL;
+    this.imageURL = imageURL;
+    this.imageCaption = imageCaption;
+    this.imageDelay = imageDelay;
+
+    this.isSelected = false;
+    this.readyMode = [0, 1, 2, 3, 4, 5].indexOf(readyMode) === -1 ? 0 : readyMode;
+
+    this.checkbox = null;
+    this.isImageLoaded = false;
+}
+UIElementInteractiveDelayedImageSelectable.prototype = Object.create(UIElementInteractive.prototype);
+UIElementInteractiveDelayedImageSelectable.prototype.constructor = UIElementInteractiveDelayedImageSelectable;
+
+UIElementInteractiveDelayedImageSelectable.prototype.createUI = function() {
+    this.node = document.createElement("span");
+    this.node.className = this.className;
+
+    this.checkbox = document.createElement("input");
+    this.checkbox.type = "checkbox";
+    this.node.appendChild(this.checkbox);
+    //Apply value to UI
+    this.checkbox.checked = this.isSelected;
+
+    var image = new Image();
+    image.alt = this.imageCaption;
+    //Load delay for the image
+    if (this.imageDelay > 0) {
+        var imageURL = this.imageURL;
+        image.src = this.loadAnimationURL;
+        setTimeout(
+            function() {
+                image.src = imageURL;
+                this.isImageLoaded = true;
+            }.bind(this),
+            this.imageDelay
+        );
+    } else {
+        image.src = this.imageURL;
+    }
+    this.node.appendChild(image);
+
+    image.addEventListener("click", this._onSelected.bind(this));
+    this.checkbox.addEventListener("changed", this._onSelected.bind(this));
+    this.node.addEventListener("click", this._onSelected.bind(this));
+
+    this.uiCreated = true;
+
+    return this.node;
+};
+
+UIElementInteractiveDelayedImageSelectable.prototype.releaseUI = function() {
+    this.node = null;
+    this.uiCreated = false;
+    this.enabled = false;
+
+    this.checkbox = null;
+    this.isImageLoaded = false;
+};
+
+UIElementInteractiveDelayedImageSelectable.prototype.isReady = function() {
+    switch (this.readyMode) {
+        case 0:
+            return true;
+        case 1:
+            return this.isSelected;
+        case 2:
+            return this.isSelected === false;
+        case 3:
+            return this.isImageLoaded;
+        case 4:
+            return this.isImageLoaded && this.isSelected;
+        case 5:
+            return this.isImageLoaded && this.isSelected === false;
+    }
+};
+
+UIElementInteractiveDelayedImageSelectable.prototype._onSelected = function(event) {
+    if (!this.isUIcreated()) return;
+
+    if ([4, 5].indexOf(this.readyMode) != -1 && !this.isImageLoaded) return;
+
+    this.isSelected = !this.isSelected;
+    this.checkbox.checked = this.isSelected;
+
+    event.stopPropagation();
+};
