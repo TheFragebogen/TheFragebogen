961bb5c152ed922321a33928d5227f2b1d236d52
Konrad Wolsing <konrad.richard.wolsing@rwth-aachen.de>
2019-05-10 14:15:07 +0200

    Audio/Video: removed src attribute

--- src/questionnaireitem_media_video.js
+++ src/questionnaireitem_media_video.js
@@ -66,8 +66,6 @@ QuestionnaireItemMediaVideo.prototype._createMediaNode = function() {
         this.videoNode.appendChild(videoSource);
     }
 
-    this.videoNode.src = this.url;
-
     pTag = document.createElement("p");
     pTag.innerHTML = "This is a fallback content. Your browser does not support the provided video formats.";
     this.videoNode.appendChild(pTag);
